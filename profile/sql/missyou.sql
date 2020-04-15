e98e8be0950cd4cd4b9c02012dacd4afdaa8b42e
JackEasons <yixiaowu2008@gmail.com>
2020-04-16 00:51:55 +0800

    Update and rename missyou-v1.sql to missyou.sql

--- profile/sql/missyou.sql
+++ profile/sql/missyou.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 7yue
+ Source Server         : sleeve
  Source Server Type    : MySQL
  Source Server Version : 80016
  Source Host           : localhost:3306
